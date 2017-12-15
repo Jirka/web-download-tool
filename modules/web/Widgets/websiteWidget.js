70b75fd1932ca9948fd1b8551470c29459a2619e
Adriana Jelencikova <adrianajelencikova@gmail.com>
2017-12-15 18:26:04 +0100

    Added module for general website processing

---
+++ modules/web/Widgets/websiteWidget.js
@@ -0,0 +1,53 @@
+function websiteWidget()
+{
+	
+	this.init = function(html, config, level)
+	{
+		var generalWidget = new Widget(console);
+		generalWidget.init(html, config, level);
+		this.generalWidget = generalWidget;
+
+		//self instance --should be instance? widow[config.customWidget] ; new this.object();
+		this.object = config.customWidget;
+	}
+
+	this.execute = function()
+	{
+		this.generalWidget.setCoordinates();
+		this.generalWidget.setSubwidgets(this.getSubwidgets());
+	}
+	
+	this.getSubwidgets = function(){
+		var subwidgets = [];
+		if(this.generalWidget.level <= this.generalWidget.maxLevel){
+			// console.log('level::'+this.generalWidget.level+' max::'+this.generalWidget.maxLevel);
+			for(var i in this.generalWidget.html.children){
+				if(typeof this.generalWidget.html.children[i] === 'object'){
+					
+					var widget = new window[this.object](console);
+					console.log('creating subwidget'+this.generalWidget.html.children[i]);
+					widget.init(
+						this.generalWidget.html.children[i], 
+						this.generalWidget.config, 
+						this.generalWidget.level+1
+					);
+					widget.execute();
+					subwidgets.push(widget.getWidget());
+				}
+				else{
+						//just for debug
+						// console.log(this.html.children[i]);
+				}
+			}
+		}
+		return subwidgets;
+	};
+
+	/*
+	* forwarding instance of general widget
+	*/
+	this.getWidget = function()
+	{
+		return this.generalWidget;
+	}
+};
