c8099fa0ef70d5b8a134510b089a0622fa8820a6
adriana <adrianajelencikova@gmail.com>
2018-03-16 23:56:20 +0100

    Added sorting generated samples into subfolders

--- down.sh
+++ down.sh
@@ -7,25 +7,49 @@ START_INDEX=0   						#number of line to start  (while reading from `LIST_OF_URL
 ITERATIONS=0 							#max number of iterations, 0 = all
 WIDTH=1000								#width of browser window
 HEIGHT=1000								#height of browser window
-TIMEOUT=20000							#timeout in ms
+TIMEOUT=3000							#timeout in ms
+CLEAR_STORAGE=true						#flag to delete all files and directories inside result path
+
+SERVICES=('klipfolio' 'clicdata' 'datapine')
+SERVICE_NAME='other'
 
 #initialization
 I=0
 COUNT=0
 WEBPAGE=''
 
+
+if [ ${CLEAR_STORAGE} = true ]; then
+	rm -r "$RESULTPATH"
+fi
+
 while read -r line; do
 
 	if [[ ( "$I" -ge "$START_INDEX" ) && ( ( "$ITERATIONS" -eq 0 ) || ( "$COUNT" -le "$ITERATIONS" ) ) ]]; then 
 		WEBPAGE="$line"
-		
-		NEWRESULTPATH=$RESULTPATH$COUNT
+
+		for SERVICE in ${SERVICES[@]}; do
+			if [[ ${WEBPAGE} == *".$SERVICE."* ]]; then
+				SERVICE_NAME="$SERVICE"
+			fi
+		done
+
+		SERVICE_NAME="$SERVICE_NAME/"
+		NEWRESULTPATH=$RESULTPATH$SERVICE_NAME
+
+		SERVICE_COUNT=0	
+		if [ -d ${NEWRESULTPATH} ]; then
+			SERVICE_COUNT=`ls -l ${NEWRESULTPATH} | grep -c ^d`
+		fi
+
+		NEWRESULTPATH=$NEWRESULTPATH$SERVICE_COUNT
 		
 		#generate JSON configuration file
-		printf '{"width":%d,"height":%d,"resultPath":"%s", "wrap":true, "timeout" : %d}\n' "$WIDTH" "$HEIGHT" "$NEWRESULTPATH" "$TIMEOUT" > "$CONFIGPATH"
+		printf '{"url" : "%s", "width":%d,"height":%d,"resultPath":"%s", "wrap":true, "timeout" : %d}\n' "$WEBPAGE" "$WIDTH" "$HEIGHT" "$NEWRESULTPATH" "$TIMEOUT" > "$CONFIGPATH"
 
 		#run program
-		phantomjs main.js ${WEBPAGE} -c "$CONFIGPATH"
+		phantomjs main.js -c "$CONFIGPATH"
+		break
 
 		((COUNT++))
 	fi
