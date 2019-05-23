42da72484d51e07bab565d7eb10520e7499bfb6a
Jiri Hynek <xhynek@gmail.com>
2019-05-23 09:19:42 +0200

    prepare.sh : phantomjs download

--- prepare.sh
+++ prepare.sh
@@ -3,4 +3,9 @@ cd external
 npm install color-convert
 npm install css-background-parser
 cd ..
-chmod +x phantomjs
+echo "downloading phantom.js"
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2
+mv phantomjs-2.1.1-linux-x86_64 phantomjs
+chmod +x phantomjs/bin/phantomjs
+rm phantomjs-2.1.1-linux-x86_64.tar.bz2
